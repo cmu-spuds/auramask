1ed7efcbd007442a28d0c3cccee056feb3acf61f
logasja <logasja@gatech.edu>
2024-02-23 20:05:01 +0000

    Get rid of 'optimizations' made by tf

--- auramask/losses/__init__.py
+++ auramask/losses/__init__.py
@@ -1,6 +1,14 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+# from keras.losses import Loss
+# class ModelLoss(Loss):
+#   def __init__(self, model, name="ModelLoss", *args, **kwargs):
+#     super().__init__(name=name, **kwargs)
+#     self.model = model
+
+#   def call(self, y_true, y_pred):
+#     raise NotImplementedError
 
 from auramask.losses.perceptual import PerceptualLoss
 from auramask.losses.embeddistance import EmbeddingDistanceLoss
