a70c1b7550acc3077b5c6a34be490a2385d46498
logasja <logasja@gatech.edu>
2024-03-03 17:51:31 +0000

    Run with arcface

--- train_set.sh
+++ train_set.sh
@@ -1,7 +1,7 @@
 source /opt/miniconda/bin/activate unet
 export TF_CPP_MIN_LOG_LEVEL="2"
-python train.py --no-note -v 2 -E 100 --t_split train[0:2048] --v_split train[2049:2176] -F arcface -B 32 --n_filters 32 --depth 3 -L none --eager -a 0.002 || { echo 'Training failed' ; exit 1; }
-python train.py --no-note -v 2 -E 100 --t_split train[0:2048] --v_split train[2049:2176] -F arcface -B 32 --n_filters 64 --depth 3 -L none --eager -a 0.002 || { echo 'Training failed' ; exit 1; }
-python train.py --no-note -v 2 -E 100 --t_split train[0:2048] --v_split train[2049:2176] -F arcface -B 32 --n_filters 16 --depth 5 -L none --eager -a 0.002 || { echo 'Training failed' ; exit 1; }
-python train.py --no-note -v 2 -E 100 --t_split train[0:2048] --v_split train[2049:2176] -F arcface -B 32 --n_filters 16 --depth 5 -L none --eager -a 0.002 || { echo 'Training failed' ; exit 1; }
-python train.py --no-note -v 0 -E 500 --split train -F arcface -B 32 --n_filters 64 --depth 5 || { echo 'Training failed' ; exit 1; }
+python train.py --no-note -v 0 -E 500 --t_split train[0:2048] --v_split train[2049:2176] -F arcface -B 32 --n_filters 32 --depth 3 -L none || { echo 'Training failed' ; exit 1; }
+python train.py --no-note -v 0 -E 500 --t_split train[0:2048] --v_split train[2049:2176] -F arcface -B 32 --n_filters 64 --depth 3 -L none || { echo 'Training failed' ; exit 1; }
+python train.py --no-note -v 0 -E 500 --t_split train[0:2048] --v_split train[2049:2176] -F arcface -B 32 --n_filters 16 --depth 5 -L none || { echo 'Training failed' ; exit 1; }
+python train.py --no-note -v 0 -E 500 --t_split train[0:2048] --v_split train[2049:2176] -F arcface -B 32 --n_filters 32 --depth 5 -L none || { echo 'Training failed' ; exit 1; }
+python train.py --no-note -v 0 -E 500 --t_split train[0:2048] --v_split train[2049:2176] -F arcface -B 32 --n_filters 64 --depth 5 -L none || { echo 'Training failed' ; exit 1; }
