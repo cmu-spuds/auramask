Fix SSIM for YUV colorspace, conversion of colorspace transformation to allow for gradients

auramask/losses/ssim.py
@@ -1,6 +1,48 @@
 from keras.losses import Loss
 import tensorflow as tf
+from tensorflow.python.ops.image_ops_impl import (
+    _ssim_per_channel,
+    _verify_compatible_image_shapes,
+)
 
+
+def ssim(
+    img1,
+    img2,
+    max_val,
+    filter_size=11,
+    filter_sigma=1.5,
+    k1=0.01,
+    k2=0.03,
+    return_index_map=False,
+    channel_weights=[1.0, 1.0, 1.0],
+):
+    # Convert to tensor if needed.
+    img1 = tf.convert_to_tensor(img1, name="img1")
+    img2 = tf.convert_to_tensor(img2, name="img2")
+    channel_weights = tf.convert_to_tensor(channel_weights, name="c_weights")
+    # Shape checking.
+    _, _, checks = _verify_compatible_image_shapes(img1, img2)
+    img1 = tf.identity(img1)
+
+    # Need to convert the images to float32.  Scale max_val accordingly so that
+    # SSIM is computed correctly.
+    max_val = tf.cast(max_val, img1.dtype)
+    max_val = tf.image.convert_image_dtype(max_val, tf.float32)
+    img1 = tf.image.convert_image_dtype(img1, tf.float32)
+    img2 = tf.image.convert_image_dtype(img2, tf.float32)
+    ssim_per_channel, _ = _ssim_per_channel(
+        img1,
+        img2,
+        max_val,
+        filter_size,
+        filter_sigma,
+        k1,
+        k2,
+        return_index_map,
+    )
+
+    return tf.reduce_sum(ssim_per_channel * channel_weights, [-1]) / tf.reduce_sum(channel_weights)
 
 class SSIMLoss(Loss):
     def __init__(
@@ -19,10 +61,37 @@ class SSIMLoss(Loss):
             "filter size": self.fz.numpy(),
             "k1": self.k1.numpy(),
             "k2": self.k2.numpy(),
+            "color weights": (1., 1., 1.)
         }
 
     def call(self, y_true, y_pred):
-        ssim = 1 - tf.image.ssim(
-            y_true, y_pred, max_val=self.mv, filter_size=self.fz, k1=self.k1, k2=self.k2
+        loss = 1 - ssim(
+            y_true, 
+            y_pred, 
+            max_val=self.mv, 
+            filter_size=self.fz, 
+            k1=self.k1, 
+            k2=self.k2,
+            channel_weights=[1.,1.,1.]
         )
-        return ssim
+        return loss
+
+
+class YUVSSIMLoss(SSIMLoss):
+    def get_config(self):
+        tmp = super().get_config()
+        tmp['channel_weights'] = (0.8,0.1,0.1)
+        return tmp
+
+    def call(self, y_true, y_pred):
+        loss = 1 - ssim(
+            y_true, 
+            y_pred, 
+            max_val=self.mv, 
+            filter_size=self.fz, 
+            k1=self.k1,
+            k2=self.k2,
+            channel_weights=[0.8,0.1,0.1] # Weights as described in https://doi.org/10.48550/arXiv.2101.06354
+        )
+
+        return loss

auramask/models/auramask.py
@@ -126,8 +126,8 @@ class AuraMask(Model):
         del x
         del sample_weight
         tloss = tf.constant(0, dtype=tf.float32)                            # tracked total loss
-        y_rgb = tf.stop_gradient(self.colorspace[1](y))                     # computed rgb representation
-        y_pred_rgb = tf.grad_pass_through(self.colorspace[1])(y_pred)       # computed rgb representation (with gradient passthrough i.e., identity in backward pass)
+        y_rgb = self.colorspace[1](y)                                       # computed rgb representation
+        y_pred_rgb = self.colorspace[1](y_pred)                             # computed rgb representation (with gradient passthrough only for hsv_to_rgb
 
         changed_fn = lambda: (y_rgb, y_pred_rgb)
         unproc_fn = lambda: (y, y_pred)
@@ -179,7 +179,7 @@ class AuraMask(Model):
 
         with tf.GradientTape() as tape:
             X = self.colorspace[0](X)
-            y = tf.stop_gradient(self.colorspace[0](y))
+            y = self.colorspace[0](y)
             y_pred, _ = self(X, training=True)  # Forward pass
             loss = self.compute_loss(y=y, y_pred=y_pred)  # Compute loss
 

auramask/utils/colorspace.py
@@ -7,8 +7,21 @@ def rgb(X):
     return X
 
 
+@tf.function
+def hsv_to_rgb(X):
+    """Helper function to convert from hsv to rgb while passing through the gradient as
+    it has not been implmented in a differentiable way.
+
+    Args:
+        X (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return tf.grad_pass_through(tf.image.hsv_to_rgb)(X)
+
 class ColorSpaceEnum(Enum):
     RGB = (rgb, rgb)
-    HSV = (tf.image.rgb_to_hsv, tf.image.hsv_to_rgb)
+    HSV = (tf.image.rgb_to_hsv, hsv_to_rgb)
     YIQ = (tf.image.rgb_to_yiq, tf.image.yiq_to_rgb)
     YUV = (tf.image.rgb_to_yuv, tf.image.yuv_to_rgb)